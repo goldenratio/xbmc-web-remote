91fbcdc4eb66a4f8e5ad321bf3b676b82180994a
Karthik <karthikeyanvj@gmail.com>
2013-07-20 03:05:50 +0300

    - new build

--- remote/js/enum.js
+++ remote/js/enum.js
@@ -1,4 +1,4 @@
 /* (c) 2013 Karthikeyan VJ https://github.com/goldenratio/xbmc-web-remote */
-var PORT_DEFAULT=9090;var ENABLE_CONSOLE=true;var RECONNECT_TIME_DELAY=5000;var ALLOW_POPOUT=false;var PORT_ERROR_MESSAGE="Port $port is a typical HTTP Port, but remote uses TCP Port (default 9090) to connect.";var Key=new function()
+var PORT_DEFAULT=9090;var ENABLE_CONSOLE=false;var RECONNECT_TIME_DELAY=5000;var ALLOW_POPOUT=false;var PORT_ERROR_MESSAGE="Port $port is a typical HTTP Port, but remote uses TCP Port (default 9090) to connect.";var Key=new function()
 {this.UP=38;this.DOWN=40;this.RIGHT=39;this.LEFT=37;this.SPACE=32;this.BACKSPACE=8;this.ENTER=13;this.ESCAPE=27;this.CTRL=17;this.INFO=73;this.STOP=83;this.MENU=72;this.OSD=90;this.MUTE=77;this.PLAY=80;this.CONTEXT=79;};var SettingsElementID=new function()
 {this.MISSING_DATA="message_missing_data";this.MESSAGE_SUCCESS="message_success";this.MESSAGE_FAIL="message_fail";this.MESSAGE_WAIT="message_wait";this.MESSAGE_PORT_ERROR="port_msg";this.IP_TEXTFIELD="ipValue";this.PORT_TEXTFIELD="portValue";this.SAVE_BUTTON="saveButton";this.BACK_BUTTON="backButton";};
