a7a95db0500c93f7faa7a58ab3f403576f8c8258
Karthik <karthikeyanvj@gmail.com>
2015-02-18 22:10:25 +0200

    - updated firefox addon sdk to 1.17

--- res/firefox_addon/lib/main.js
+++ res/firefox_addon/lib/main.js
@@ -1,24 +1,40 @@
 /**
  * Firefox addon
  * @author Karthik VJ
+ * @source https://developer.mozilla.org/en-US/Add-ons/SDK/High-Level_APIs/panel
  */
 
 
-var data = require("sdk/self").data;
-var widgets = require("sdk/widget");
-var tabs = require("sdk/tabs");
+var { ToggleButton } = require('sdk/ui/button/toggle');
 var panels = require("sdk/panel");
+var self = require("sdk/self");
 
-// Construct a panel
-var remote_panel = panels.Panel({
+var button = ToggleButton({
+    id: "kassi-remote-button",
+    label: "Kassi",
+    icon: {
+        "16": "./icons/16.png",
+        "32": "./icons/32.png",
+        "64": "./icons/64.png"
+    },
+    onChange: handleChange
+});
+
+var panel = panels.Panel({
     width: 340,
     height: 520,
-    contentURL: data.url("remote.html")
+    contentURL: self.data.url("remote.html"),
+    onHide: handleHide
 });
 
-var widget = widgets.Widget({
-    id: "xbmc-remote",
-    label: "XBMC Remote",
-    contentURL: data.url("icons/16.png"),
-    panel: remote_panel
-});
+function handleChange(state) {
+    if (state.checked) {
+        panel.show({
+            position: button
+        });
+    }
+}
+
+function handleHide() {
+    button.state('window', {checked: false});
+}
